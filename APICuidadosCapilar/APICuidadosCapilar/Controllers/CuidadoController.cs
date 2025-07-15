8ee3fd5eec6dbcfed9696c16b19400c7a2c097dc
Danilo <daniloarrabal1@hotmail.com>
2025-07-15 13:10:44 -0300

    CuidadoController

---
+++ APICuidadosCapilar/APICuidadosCapilar/Controllers/CuidadoController.cs
@@ -0,0 +1,97 @@
+﻿using APICuidadosCapilar.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Models.CuidadosCapilar.Model;
+
+namespace APICuidadosCapilar.Controllers
+{
+    public class CuidadoController : ControllerBase
+    {
+        RepositoryCuidado _repositoryCuidado;
+        private readonly DBRotinaCapilarContext _context;
+
+        public CuidadoController(DBRotinaCapilarContext context)
+        {
+            _repositoryCuidado = new RepositoryCuidado();
+            _context = context;
+        }
+
+        //lista Cuidados
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cuidado>>> ListaCuidados()
+        {
+            try
+            {
+                var cuidados = await _repositoryCuidado.SelecionarTodosAsync();
+                return Ok(cuidados);
+            }
+            catch
+            {
+                return BadRequest("Erro ao retornar lista de cuidados");
+            }
+        }
+
+        //escolhe Cuidado
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cuidado>> GetCuidado(int id)
+        {
+            try
+            {
+                var cuidado = await _repositoryCuidado.SelecionarPkAsync(id);
+                return Ok(cuidado);
+            }
+            catch
+            {
+                return BadRequest("Erro ao retornar cuidado");
+            }
+        }
+
+        //adiciona Cuidado
+        [HttpPost]
+        public async Task<ActionResult<Cuidado>> AddCuidado(Cuidado cuidado)
+        {
+            try
+            {
+                cuidado.DataCriacao = DateTime.Now;
+                cuidado.DataModificacao = DateTime.Now;
+                await _repositoryCuidado.IncluirAsync(cuidado);
+                return Ok("Cuidado adicionado");
+            }
+            catch
+            {
+                return BadRequest("Erro ao adicionar cuidado");
+            }
+        }
+
+        //edita Cuidado
+        [HttpPut]
+        public async Task<ActionResult<Cuidado>> EditLavagem(Cuidado cuidado)
+        {
+            try
+            {
+                cuidado.DataModificacao = DateTime.Now;
+                await _repositoryCuidado.AlterarAsync(cuidado);
+                return Ok("Cuidado editado");
+            }
+            catch
+            {
+                return BadRequest("Erro ao editar cuidado");
+            }
+        }
+
+        //exclui Cuidado
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Cuidado>> DeleteCuidado(int id)
+        {
+            try
+            {
+                var cuidado = await _repositoryCuidado.SelecionarPkAsync(id);
+                await _repositoryCuidado.ExcluirAsync(cuidado);
+                return Ok("Cuidado excluida");
+            }
+            catch
+            {
+                return BadRequest("Erro ao excluir cuidado");
+            }
+        }
+    }
+}
--- APICuidadosCapilar/APICuidadosCapilar/obj/Debug/net9.0/APICuidadosCapilar.AssemblyInfo.cs
+++ APICuidadosCapilar/APICuidadosCapilar/obj/Debug/net9.0/APICuidadosCapilar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICuidadosCapilar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca53e53c4b9c9ca162b54bbc2e0f40b9d942aa9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30fbf0a76c23680b8294d68b1f94b4df74054f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICuidadosCapilar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICuidadosCapilar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
